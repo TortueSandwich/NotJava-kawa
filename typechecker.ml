957754f614650e61de6f88ef58cff12a23b6fda4
TortueSandwich <artsaillant@gmail.com>
2025-01-09 18:42:03 +0100

    ajout de todo

--- typechecker.ml
+++ typechecker.ml
@@ -84,6 +84,7 @@ let find_method_def meth_name methods =
   match List.find_opt (fun m -> m.method_name = meth_name) methods with
   | Some m -> m
   | None -> 
+    (* TODO !! *)
     error ("Method not found: " ^ meth_name ^ (match closest_string meth_name (List.map (fun m -> m.method_name) methods) with
      | Some closest -> ", did you mean " ^ closest ^ " ?\n"
      | None -> ""))
@@ -208,7 +209,7 @@ let typecheck_prog (p : program) : program =
         let typcls = objname_of_typ typed_obj.annot in
         let defclass = find_class_def typcls in
         let definterfaces = get_interfaces_from_class typcls in 
-
+        (* FAIRE fonction getallmethod classdef -> method list *)
         let what = (defclass.methods@(List.filter (fun x -> x.default = true) (List.flatten (List.map (fun inter-> inter.methods) definterfaces)))) in
         let methodeu = find_method_def meth_name what in
         let param_types = List.map snd methodeu.params in
